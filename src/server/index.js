e94ec613a0ef87110895ae3c3208d2e4c7a4486c
April <aprilnicole013@gmail.com>
2021-08-20 20:59:16 -0400

    Pull in most recent image from one mars rover

--- src/server/index.js
+++ src/server/index.js
@@ -14,30 +14,30 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
-//get rover image
-app.get('/manifests/:roverName', async (req, res) => {
+//get rover data
+app.get('/rovers/:name/photos', async (req, res) => {
     try {
-        const { roverName } = req.params;
-        let roverDetails = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?max_date&api_key=${process.env.API_KEY}`)
+        const rover = req.params.name;
+        let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ roverDetails })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
-
-
-
-
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+            res.send({ roverPhotos })
     } catch (err) {
         console.log('error:', err);
     }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+// example API call
+// app.get('/apod', async (req, res) => {
+//     try {
+//         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+//             .then(res => res.json())
+//         res.send({ image })
+//     } catch (err) {
+//         console.log('error:', err);
+//     }
+// })
